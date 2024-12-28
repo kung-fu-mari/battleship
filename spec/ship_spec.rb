31d285596670dbab3de73ff3e69d83af39dfe39b
kung-fu-mari <ifcodes@duck.com>
2024-12-27 19:14:39 -0700

    feat: add test bodies

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,10 +6,23 @@ RSpec.describe Ship do
   end
 
   it 'has attributes' do
-    
+    expect(cruiser.name).to eq("Cruiser")
+    expect(cruiser.length).to eq(3)
+    expect(cruiser.health).to eq(3)
   end
 
   it 'can be sunk' do
+    expect(cruiser.health).to eq(3)
+    expect(cruiser.sunk?).to eq(false)
 
+    cruiser.hit
+    expect(cruiser.health).to eq(2)
+
+    cruiser.hit
+    expect(cruiser.health).to eq(1)
+    expect(cruiser.sunk?).to eq(false)
+
+    cruiser.hit
+    expect(cruiser.sunk?).to eq(true)
   end 
 end
