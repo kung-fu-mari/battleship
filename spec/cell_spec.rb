11e41ccc184942aaae131bb6856848260ddb732b
kung-fu-mari <ifcodes@duck.com>
2024-12-29 16:19:42 -0700

    feat: add renders itself test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -28,6 +28,28 @@ RSpec.describe Cell do
   end 
 
   it 'renders itself' do
-    
+    cell_1 = Cell.new("B4")
+    expect(cell_1.render).to eq(".")
+    cell_1.fire_upon
+    # M for missed shot; no ship
+    expect(cell_1.render).to eq("M")
+
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    # ship is hidden by default
+    expect(cell_2.render).to eq(".")
+    # passing true to render shows (S)hip if present
+    expect(cell_2.render(true)).to eq("S")
+    cell_2.fire_upon
+    # if a ship is (H)it, it is shown automatically
+    expect(cell_2.render).to eq("H")
+    expect(cruiser.sunk?).to eq(false)
+
+    # When a ship has been sunk, we show an X
+    cruiser.hit
+    cruiser.hit
+    expect(cruiser.sunk?).to eq(true)
+    expect(cell_2.render).to eq("X")
   end
 end
