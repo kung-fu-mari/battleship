34e45d99058f9396e6c4a2793f83da76d2255366
kung-fu-mari <ifcodes@duck.com>
2024-12-28 17:30:42 -0700

    fix: remove before :each block

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,17 +2,15 @@ require './lib/ship'
 require './lib/cell'
 
 RSpec.describe do
-  before :each do
-    cell = Cell.new("B4")
-  end
-
   it 'has placement data' do
+    cell = Cell.new("B4")
     expect(cell.coordinate).to eq("B4")
     expect(cell.ship).to eq(nil)
     expect(cell.empty?).to eq(true)
   end
 
   it 'can hold a ship segment' do
+    cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
     expect(cell.ship).to eq(cruiser)
