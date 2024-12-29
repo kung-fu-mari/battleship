72963f46bbbe45c8e29d0e5d6daa8f6d7fa483ca
kung-fu-mari <ifcodes@duck.com>
2024-12-29 02:17:38 -0700

    feat: add lines

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,7 +1,7 @@
 require './lib/ship'
 require './lib/cell'
 
-RSpec.describe do
+RSpec.describe Cell do
   it 'has placement data' do
     cell = Cell.new("B4")
     expect(cell.coordinate).to eq("B4")
@@ -17,4 +17,17 @@ RSpec.describe do
     expect(cell.empty?).to eq(false)
   end
   
+  it 'damages its ship when it is fired upon' do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    expect(cell.fired_upon?).to eq(false)
+    cell.fire_upon
+    expect(cell.ship.health).to eq(2)
+    expect(cell.fired_upon?).to eq(true)
+  end 
+
+  it 'renders itself' do
+    
+  end
 end
