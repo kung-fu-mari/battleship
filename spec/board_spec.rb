f3041e0ae01857f8b75577dea3b10138f2836060
kung-fu-mari <ifcodes@duck.com>
2024-12-31 19:23:21 -0700

    feat: add final tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,39 +8,51 @@ RSpec.describe Board do
   end
   
   it 'can tell if a space is on the board' do
+    board = Board.new
+
     expect(board.valid_coordinate?("A1")).to eq(true)
     expect(board.valid_coordinate?("D4")).to eq(true)
     expect(board.valid_coordinate?("A5")).to eq(false)
     expect(board.valid_coordinate?("E1")).to eq(false)
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
-  
-  describe '#places ships correctly' do
-    before(:each) do
-      cruiser = Ship.new("Cruiser", 3)
-      submarine = Ship.new("Submarine", 2)
-    end
 
-    it "ship length must be same as number of coordinates" do
-      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
-    end
 
-    it "coordinates must be consecutive" do
-      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
-      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-    end
+  it "ship length must be same as number of coordinates" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
 
-    it "coordinates cannot be diagonal" do
-      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
-    end
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  it "coordinates must be consecutive" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+  end
+
+  it "coordinates cannot be diagonal" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+  end
     
-    it "valid placements" do
-      expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
-      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(false)
-    end
+  it "valid placements" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("CATCH ERRORS!!", 2)
+
+    expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
 end
